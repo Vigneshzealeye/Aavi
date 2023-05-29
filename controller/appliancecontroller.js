2479c22bf16640396461b887546e0da7d6a5222f
Vigneshzealeye <131443897+Vigneshzealeye@users.noreply.github.com>
2023-05-29 13:20:30 +0530

    twin v BE

--- controller/appliancecontroller.js
+++ controller/appliancecontroller.js
@@ -1,21 +1,20 @@
 var Appliance = require("../model/Appliances");
 var Device = require("../model/Device");
-
+var to = 50;
 exports.createappliance = async (req, res) => {
   try {
-    var email=req.email
-    console.log(email)
-    var { applianceName, device_id ,nodeIndex} = req.body;
+    var email = req.email;
+    console.log(email);
+    var { applianceName, device_id, nodeIndex } = req.body;
     //console.log(device_id)
     var newAppliance = {
       applianceName,
       nodeIndex,
-      isAssigned:true,
-      capabilities:(req.dimmer&&nodeIndex==0)?"regulator":"switch",
-      userMail:email,
-      controller_id:device_id
-      
-      };
+      isAssigned: true,
+      capabilities: req.dimmer && nodeIndex == 0 ? "regulator" : "switch",
+      userMail: email,
+      controller_id: device_id,
+    };
     var appliance = await new Appliance(newAppliance).save();
 
     await Device.updateOne(
@@ -25,19 +24,19 @@ exports.createappliance = async (req, res) => {
 
     res.status(200).json(appliance);
   } catch (error) {
-    console.log("Appliance error:"+error);
+    console.log("Appliance error:" + error);
     res.status(500).json({ message: "Something Went Wrong" });
-  } 
+  }
 };
 
 exports.editappliancename = async (req, res) => {
   try {
-    var email=req.email
+    var email = req.email;
 
     var { id, newName } = req.body;
 
     await Appliance.updateOne(
-      { _id: id,userMail:email },
+      { _id: id, userMail: email },
       { $set: { applianceName: newName } }
     );
 
@@ -50,95 +49,106 @@ exports.editappliancename = async (req, res) => {
 
 exports.deleteAppliance = async (req, res) => {
   try {
-    var email=req.email
+    var email = req.email;
     var { id } = req.body;
 
-    await Appliance.deleteOne({ _id: id,userMail:email });
+    await Appliance.deleteOne({ _id: id, userMail: email });
 
     //await Device.updateOne({"appliances.appliance_id":id},{$pull:{appliances:{appliance_id:id}}})
 
-    res.status(200).json({message:"Deletion Success"});
+    res.status(200).json({ message: "Deletion Success" });
   } catch (error) {
     console.log(error);
     res.status(500).json("Something Went Wrong");
   }
 };
-exports.toggle=async(req,res)=>{
+exports.toggle = async (req, res) => {
   try {
-    
-    var{switchingstate,id}=req.body
+    var { switchingstate, id } = req.body;
 
-    var nodeStatus=switchingstate[2].toString()==0?false:true
+    var nodeStatus = switchingstate[2].toString() == 0 ? false : true;
 
-    await Appliance.updateOne({_id:id},{$set:{nodeStatus:nodeStatus}})
+    await Appliance.updateOne(
+      { _id: id },
+      { $set: { nodeStatus: nodeStatus } }
+    );
 
-    res.status(200).json("Toggle Success")
-
-    
-  
-  } 
-  
-  catch (error) {
-    res.status(500).json("Something went wrong")
+    res.status(200).json("Toggle Success");
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
-  
-}
+};
 
-
-
-
-
-
-exports.getApplianceByIdandEmail=async(req,res)=>{
-
-  try{
-    var {mail,id}=req.query
-    var endpoint={}
-    console.log("mail:"+mail)
-    console.log("id:"+id)
-    var app=await Appliance.findOne({_id:id,userMail:mail})
-    // console.log(app)
+exports.getApplianceByIdandEmail = async (req, res) => {
+  try {
+    var { mail, id } = req.query;
+    var endpoint = {};
+    console.log("mail:" + mail);
+    console.log("id:" + id);
+    var app = await Appliance.findOne({ _id: id, userMail: mail });
+    console.log(app);
     //endpoint=app.controller_id.toString()+app.nodeIndex.toString()
-    endpoint["Topic"]=app.controller_id
-    endpoint["node"]=app.nodeIndex
-    res.status(200).json(endpoint)
+    endpoint["Topic"] = app.controller_id;
+    endpoint["node"] = app.nodeIndex;
+    // return res.status(200).json(endpoint)
 
- 
+    console.log(endpoint);
+    return res.status(200).json(endpoint);
+  } catch (error) {
+    console.log("Appliance Error: " + error);
+    res.status(500).json("something went wrong");
   }
-  catch(error){
-    console.log("Appliance Error: "+error)
-    res.status(500).json("something went wrong")
-  }
-
-  
-
-  
-}
+};
 
 exports.getAlldevicesofUserbyMailid = async (req, res) => {
   try {
-    var email=req.params.email
-    console.log("email:"+email);
+    var email = req.email;
+    console.log("email:" + email);
 
     let devices = await Device.find({ device_owner: email });
     // console.log(Object.getOwnPropertySymbols(devices))
-    var op=devices.map(async(e)=>{
-     var app=await e.populate("appliances")
-     
-     return app
-     
-    })
+    var op = devices.map(async (e) => {
+      var app = await e.populate("appliances");
+
+      return app;
+    });
     // var op2=op.map(async(e)=>{
 
     // })
-   
-    Promise.all(op).then((sol)=>res.status(200).json(sol))
+
+    Promise.all(op).then((sol) => res.status(200).json(sol));
 
     //let app=await devices.populate("appliances")
-    
+
     // res.status(200).json("success");
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message:  "Something went wrong" });
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+exports.getAlldevicesofUserbyMailidAlexa = async (req, res) => {
+  try {
+    var email = req.params.email;
+    console.log("email:" + email);
+
+    let devices = await Device.find({ device_owner: email });
+    // console.log(Object.getOwnPropertySymbols(devices))
+    var op = devices.map(async (e) => {
+      var app = await e.populate("appliances");
+
+      return app;
+    });
+    // var op2=op.map(async(e)=>{
+
+    // })
+
+    Promise.all(op).then((sol) => res.status(200).json(sol));
+
+    //let app=await devices.populate("appliances")
+
+    // res.status(200).json("success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
--- routes/applianceroutes.js
+++ routes/applianceroutes.js
@@ -1,13 +1,37 @@
-var appliancecontroller=require("../controller/appliancecontroller")
-var appliancemiddleware=require("../middleware/appliancemiddleware")
-var authmiddleware=require("../middleware/authvalidator")
+var appliancecontroller = require("../controller/appliancecontroller");
+var appliancemiddleware = require("../middleware/appliancemiddleware");
+var authmiddleware = require("../middleware/authvalidator");
 
+module.exports = (app) => {
+  app.post(
+    "/addappliance",
+    [appliancemiddleware.appliancemiddleware, authmiddleware.tokenvalidator],
+    appliancecontroller.createappliance
+  );
+  app.patch(
+    "/editappliancename",
+    [authmiddleware.tokenvalidator],
+    appliancecontroller.editappliancename
+  );
+  app.delete(
+    "/deleteappliance",
+    [authmiddleware.tokenvalidator],
+    appliancecontroller.deleteAppliance
+  );
+  app.post("/toggleState", appliancecontroller.toggle);
+  app.get(
+    "/alldevicebyemail",
+    authmiddleware.tokenvalidator,
+    appliancecontroller.getAlldevicesofUserbyMailid
+  );
+  app.get(
+    "/getApplianceByIdandEmail",
 
-module.exports=(app)=>{
-    app.post("/addappliance",[appliancemiddleware.appliancemiddleware,authmiddleware.tokenvalidator],appliancecontroller.createappliance)
-    app.patch("/editappliancename",[authmiddleware.tokenvalidator],appliancecontroller.editappliancename)
-    app.delete("/deleteappliance",[authmiddleware.tokenvalidator],appliancecontroller.deleteAppliance)
-    app.post("/toggleState",appliancecontroller.toggle)
-    app.get("/alldevicebyemail/:email",appliancecontroller.getAlldevicesofUserbyMailid)
-    app.get("/getApplianceByIdandEmail",appliancecontroller.getApplianceByIdandEmail)
-}
+    appliancecontroller.getApplianceByIdandEmail
+  );
+  app.get(
+    "/getApplianceByIdandEmailAlexa/:email",
+
+    appliancecontroller.getAlldevicesofUserbyMailidAlexa
+  );
+};
